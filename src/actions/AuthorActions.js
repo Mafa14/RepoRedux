e89cb23d4af78dc48e88e137050906b7480439e4
Mafa14 <m.silva.gambardelo@gmail.com>
2017-12-13 16:51:31 -0300

    Async status and error handling added

--- src/actions/AuthorActions.js
+++ src/actions/AuthorActions.js
@@ -1,5 +1,6 @@
 import * as types from './ActionsTypes';
 import authorApi from '../api/MockAuthorApi';
+import { beginAjaxCall } from './AjaxStatusAction';
 
 export function loadAuthorsSuccess(authors) {
     return { type: types.LOAD_AUTHORS_SUCCESS, authors };
@@ -7,6 +8,7 @@ export function loadAuthorsSuccess(authors) {
 
 export function loadAuthors() {
     return function (dispatch) {
+        dispatch(beginAjaxCall());
         return authorApi.getAllAuthors().then(authors => {
             dispatch(loadAuthorsSuccess(authors));
         }).catch(error => {
