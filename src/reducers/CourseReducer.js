ba47ae2c1461e4bc325facbe1aa8a9274aca6c5b
Mafa14 <m.silva.gambardelo@gmail.com>
2017-12-13 15:48:54 -0300

    Authors and Courses Save and Edit functionlity fully functional.

--- src/reducers/CourseReducer.js
+++ src/reducers/CourseReducer.js
@@ -5,6 +5,16 @@ export default function courseReducer(state = state = initialState.courses, acti
     switch (action.type) {
         case types.LOAD_COURSES_SUCCESS:
             return action.courses;
+        case types.CREATE_COURSES_SUCCESS:
+            return [
+                ...state,
+                Object.assign({}, action.course)
+            ];
+        case types.UPDATE_COURSES_SUCCESS:
+            return [
+                ...state.filter(course => course.id !== action.course.id),
+                Object.assign({}, action.course)
+            ];
         default:
             return state;
     }
